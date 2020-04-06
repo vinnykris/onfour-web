6cefb44f81819c3038bf048438f04138b485005c
Spencer Amer <spenceramer@Spencers-MacBook-Air.local>
2020-04-06 19:42:46 -0400

    whatsonfour page, assets, and styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import MainContent from "./components/main_content";
 import "./App.css";
 import "./styles.scss";
 import background from "./images/home_page_background.jpeg";
+import WhatsOnFour from "./components/whatsonfour";
 
 function App() {
   return (
