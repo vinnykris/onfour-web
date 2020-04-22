32a40f10139f7073d07815626a7996247e492450
Vinod Krishnamurthy <vinodkrishnam@gmail.com>
2020-04-22 19:03:14 -0400

    added payment via venmo to the social bar, updated stream page styling

--- src/components/stream_page.jsx
+++ src/components/stream_page.jsx
@@ -8,6 +8,12 @@ import Chat from "./chat/stream_chat";
 import Join from "./chat/join_chat";
 import { Grid, Row, Col } from "./grid";
 import SocialBar from "./social_bar";
+import { API, graphqlOperation } from "aws-amplify";
+import * as mutations from "../graphql/mutations";
+import Amplify from "aws-amplify";
+import awsmobile from "../AppSync";
+
+Amplify.configure(awsmobile);
 
 class StreamPage extends Component {
   joinSubmit = (name, mode) => {
@@ -23,13 +29,38 @@ class StreamPage extends Component {
     });
   };
 
+  // emailSubmit = (event) => {
+  //   event.preventDefault();
+
+  //   this.setState({
+  //     email:
+  //   });
+
+  //   const payload = {
+  //     email: this.state.email,
+  //     paid: false,
+  //   };
+
+  //   API.graphql(
+  //     graphqlOperation(mutations.createOnfour_current, { input: payload })
+  //   );
+
+  //   this.setState({
+  //     email: "",
+  //     emailSubmitted: true,
+  //   });
+  // };
+
   constructor(props) {
     super(props);
     this.state = {
       isLive: false,
       showChat: false,
       chatName: "",
+      email: "",
+      emailSubmitted: false,
     };
+    var emailText = "";
   }
 
   render() {
@@ -41,19 +72,18 @@ class StreamPage extends Component {
             <Col size={7}>
               <Row>
                 <Col size={1}>
-                  <h2>SUPERFRIEND</h2>
+                  <h1>Onfour Presents: SUPERFRIEND</h1>
                 </Col>
               </Row>
               <div className="stream-main">
                 <div className="stream-wrapper">
                   <VideoPlayer
                     url={
-                      "https://d20g8tdvm6kr0b.cloudfront.net/out/v1/6ec8306782ce45fc8f1657cd08538339/CFAM_manifest/index.m3u8"
+                      "https://d20g8tdvm6kr0b.cloudfront.net/out/v1/474ceccf630440328476691e9bdeaeee/index.m3u8"
                     }
                   />
                 </div>
               </div>
-              <SocialBar />
             </Col>
             <Col className="flex-col" size={3}>
               <div className="chat-main">
@@ -68,6 +98,49 @@ class StreamPage extends Component {
                   )}
                 </div>
               </div>
+            </Col>
+            <Col size={1}></Col>
+          </Row>
+          <Row>
+            <Col size={1}></Col>
+            <Col size={7}>
+              <SocialBar />
+            </Col>
+            <Col size={3}>
+              {/* {(() => {
+                if (this.state.emailSubmitted) {
+                  return <div>Thank you and stay tuned! :)</div>;
+                } else {
+                  return (
+                    <form
+                      class="inline-form"
+                      action="/"
+                      id="newsletter"
+                      onSubmit={this.emailSubmit}
+                    >
+                      <input
+                        type="email"
+                        placeholder="Enter your email here..."
+                        name="email"
+                        required
+                        value={this.emailText}
+                        style={{ width: "350px" }}
+                        onChange={(event) =>
+                          onChange()
+                        }
+                      />
+                      <button
+                        type="submit"
+                        form="newsletter"
+                        value="Submit"
+                        style={{ width: "90px" }}
+                      >
+                        Submit
+                      </button>
+                    </form>
+                  );
+                }
+              })()} */}
             </Col>
             <Col size={1}></Col>
           </Row>
