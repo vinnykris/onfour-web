9070261c133856dd3cb2213643ed8a6e196937eb
Vinod Krishnamurthy <vinodkrishnam@gmail.com>
2020-05-04 23:43:14 -0400

    little fix

--- src/components/chat/stream_chat.jsx
+++ src/components/chat/stream_chat.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 import "./chat.scss";
 import InfoBar from "./info_bar";
