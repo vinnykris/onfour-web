d9ec29c80218e0c5c1fbc37366708e7268039c3f
Lily <wangy184@newschool.edu>
2020-05-10 22:02:21 -0400

    changed footer logo

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Grid, Row, Col } from "../grid";
 import "./footer_styles.scss";
-import white_logo from "../../images/logos/white_logo.png";
+import white_logo from "../../images/logos/logo_blackbackground.png";
 
 const Footer = () => {
   return (
