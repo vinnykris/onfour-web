110c0388f98fff559884d9036fa9a3584c36ce83
Vinod Krishnamurthy <vinodkrishnam@gmail.com>
2020-05-10 21:28:02 -0400

    user can have empty username

--- src/components/chat/join_chat.jsx
+++ src/components/chat/join_chat.jsx
@@ -6,9 +6,11 @@ const Join = ({ joinSubmit, mode }) => {
   const handleSubmit = () => {
     if (name.length > 30) {
       alert("Username cannot exceed 30 characters.");
-    } else if (name.length < 1) {
-      alert("Username cannot be empty.");
-    } else {
+    }
+    // else if (name.length < 1) {
+    //   alert("Username cannot be empty.");
+    // }
+    else {
       mode = true;
       joinSubmit(name, mode);
     }
