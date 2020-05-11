975a23cef150d42ef259d024410fa2327aeded51
Vinod Krishnamurthy <vinodkrishnam@gmail.com>
2020-05-10 20:59:50 -0400

    username must be at least one character

--- src/components/chat/join_chat.jsx
+++ src/components/chat/join_chat.jsx
@@ -6,6 +6,8 @@ const Join = ({ joinSubmit, mode }) => {
   const handleSubmit = () => {
     if (name.length > 30) {
       alert("Username cannot exceed 30 characters.");
+    } else if (name.length < 1) {
+      alert("Username cannot be empty.");
     } else {
       mode = true;
       joinSubmit(name, mode);
