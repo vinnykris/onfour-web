05335f0f168bc9fd2d045fb92299a21a80ef4c9b
Vinod Krishnamurthy <vinodkrishnam@gmail.com>
2020-05-20 17:10:11 -0700

    archive pages structural changes pt. 2

---
+++ src/components/archive_page/archive_container.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Grid, Row } from "../grid";
+
+import "./archive_styles.scss";
+
+const ArchiveContainer = ({ artistName, date }) => {
+  return <div></div>;
+};
+
+export default ArchiveContainer;
