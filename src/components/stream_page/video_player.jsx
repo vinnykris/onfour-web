a5d5c2ee8cdfb2ee9f928f0f1baa284e51d9027c
LAPTOP-F15JHGDS\16939 <yuxinz9781@gmail.com>
2020-06-07 16:16:09 -0400

    ANAYLTICS | added comments for stream page

--- src/components/stream_page/video_player.jsx
+++ src/components/stream_page/video_player.jsx
@@ -31,9 +31,18 @@ function VideoPlayer({
   // This function calculates the time difference between current time and show start time
   // and represent the difference in days, hours, minuts and seconds
   const calculateTimeLeft = () => {
-    // const difference = +new Date(start_time) - +new Date();
-    const difference =
-      +new Date("2020-06-03T19:00:00.000 - 04: 00") - +new Date();
+
+    // FOR LIVE WEBSITE: uncomment out line 38 and commment out line 43-44 
+    // to block the stream when the show hasn't started
+
+    const difference = +new Date(start_time) - +new Date();
+
+    // FOR SOUNDCHECK: comment out line 38 and uncomment out line 43-44
+    // to unblock the stream
+
+    // const difference =
+    //   +new Date("2020-06-03T19:00:00.000 - 04: 00") - +new Date();
+
     let time_left = {};
 
     if (difference > 0) {
@@ -137,11 +146,12 @@ function VideoPlayer({
     }
   }
 
+  // this function asks Amplify analytics to record the streamVisit event
   const recordEvent = () => {
     Analytics.record({ name: 'streamVisit' });
     console.log("event recorded for Analytics");
   };
-  
+
   // If the user is logged in, show them either the logged in waiting page or the
   // stream depending on the countdown
   // If the user is not logged in, show them the alternate waiting page
@@ -185,6 +195,11 @@ function VideoPlayer({
                 controls
               />
             )}
+            {/* I try to call this function here and thought it would only be called only once when the user 
+                go to the stream page AND the show is starting. However, if you look at the console logs, 
+                this function gets called for infinite times. Maybe we need to figure out a better place to 
+                call this function*/}
+            {/* {recordEvent()}  */}
           </div>
         )}
       </div>
