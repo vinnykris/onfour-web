9f64c8b6395b8f3d1d1f1b4d068fc617aa996973
LAPTOP-F15JHGDS\16939 <yuxinz9781@gmail.com>
2020-06-09 14:03:14 -0400

    STREAM | restyle the video and chat size

---
+++ src/components/video_chat/video_chat.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState, useCallback } from "react";
+// import Call from "../Call/Call";
+// import StartButton from "../StartButton/StartButton";
+// import api from "../../api";
+import "./video_chat_styles.scss";
+import Tray from "../Tray/Tray";
+// import CallObjectContext from "../../CallObjectContext";
+// import { roomUrlFromPageUrl, pageUrlFromRoomUrl } from "../../urlUtils";
+// import DailyIframe from "@daily-co/daily-js";
+// import { logDailyEvent } from "../../logUtils";
