4e01c92650580e1da06630681eb4b7e05d5266e4
Bar Kadosh <bkadosh1@Bars-MBP-3.fios-router.home>
2020-06-13 15:31:20 -0400

    added locking chat for logged out users

--- src/components/chat/input.jsx
+++ src/components/chat/input.jsx
@@ -1,5 +1,5 @@
 // React imports
-import React from "react";
+import React, { useState } from "react";
 
 // Component imports
 import ChatTooltip from "./chat_tooltip";
@@ -9,27 +9,59 @@ import "./chat.scss";
 
 // AWS imports
 import Amplify, { Analytics } from "aws-amplify";
+import Auth from "../../apis/UserPool";
 
 // Component for input field for chat
 const Input = ({ message, setMessage, sendMessage, name }) => {
+  const [auth, setAuth] = useState("");
+
+  Auth.currentAuthenticatedUser({})
+    .then((user) => {
+      setAuth(true);
+    })
+    .catch((err) => setAuth(false));
+
   return (
-    <form className="form">
-      <input
-        className="input"
-        type="text"
-        placeholder="Type a message..."
-        value={message}
-        onChange={(event) => setMessage(event.target.value)}
-        onKeyPress={(event) =>
-          event.key === "Enter" ? sendMessage(event) : null
-        }
-      />
-      <ChatTooltip title="Please login through the navigation bar.">
-        <button className="send-button" onClick={(event) => sendMessage(event)}>
-          <i class="fa fa-send"></i>
-        </button>
-      </ChatTooltip>
-    </form>
+    <div>
+      {auth ? (
+        <form className="form">
+          <input
+            className="input"
+            type="text"
+            placeholder="Type a message..."
+            value={message}
+            onChange={(event) => setMessage(event.target.value)}
+            onKeyPress={(event) =>
+              event.key === "Enter" ? sendMessage(event) : null
+            }
+          />
+          {/* <ChatTooltip title="Please login through the navigation bar."> */}
+          <button
+            className="send-button"
+            onClick={(event) => sendMessage(event)}
+          >
+            <i class="fa fa-send"></i>
+          </button>
+          {/* </ChatTooltip> */}
+        </form>
+      ) : (
+        <form className="form">
+          <input
+            className="input"
+            type="text"
+            placeholder="Please sign in to send messages!"
+            disabled
+          />
+          <button
+            className="send-button"
+            disabled={!message}
+            onClick={(event) => sendMessage(event)}
+          >
+            <i class="fa fa-send"></i>
+          </button>
+        </form>
+      )}
+    </div>
   );
 };
 
