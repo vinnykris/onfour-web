b40e005e39667242ea87fbcd238bfa9942358900
LAPTOP-F15JHGDS\16939 <yuxinz9781@gmail.com>
2020-07-27 17:18:16 -0700

    404 PAGE | restyled the 404 page

--- src/components/not_found_page/not_found_page.jsx
+++ src/components/not_found_page/not_found_page.jsx
@@ -1,27 +1,32 @@
-
-import { Link } from 'react-router-dom';
 import "../not_found_page/not_found_page_style.scss";
-import image404 from '../../images/icons/404_icon.png';
+import history from "../../history";
 var React = require('react');
 
 
-const NotFoundPage=({location})=>(
-    <div className="head"  >
-        <div className="below-head">
-        <img src={image404} className='img404' />
-        <div className='text'>
-        <h1 className='oops-heading'>
-            OOPS! Page not found
-        </h1>
-        <h3 className='error-text'>
-            The Page you are trying to reach does not exist 
-            <br/>It's probably our fault but just in case, please check 
-            <br/>that the URL you entered is correct
-        </h3>
-        <a href="/" className="go-back-button">Go Back</a>
-        </div>
-        
+const NotFoundPage= () => {
+
+    const goHome = () => {
+        history.push("/");
+    };
+
+    return (
+    <div className="not-found-page"  >
+        <div className="not-found-page-container">
+            <img src="https://onfour-media.s3.amazonaws.com/website+component/404image.png" className='not-found-page-img404' />
+            <div className='not-found-page-text'>
+                <h1 className='not-found-page-oops-heading'>
+                    OOPS! Page not found
+                </h1>
+                <h3 className='not-found-page-oops-error-text'>
+                    The Page you are trying to reach does not exist. It's probably our fault but just in case, please check that the URL you entered is correct
+                </h3>
+                <div className="not-found-page-button-wrapper">
+                    <button className="not-found-page-go-back-button" onClick={goHome}>Go Home</button>
+                </div>
+                {/* <a href="/" className="go-back-button">Go Back</a> */}
+            </div>
         </div>
     </div>
-)
+    );
+};
 export default NotFoundPage;
