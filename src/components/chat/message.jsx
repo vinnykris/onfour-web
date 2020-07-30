9257d7f0b0df2368f87cdbdba341949c75839b96
Manu Gupta <manu.isc09@mail.com>
2020-07-30 15:04:10 -0400

    Links sent in chat are now accesible as links rather than strings. '#' for twitter would be updated soon

--- src/components/chat/message.jsx
+++ src/components/chat/message.jsx
@@ -3,11 +3,19 @@ import React from "react";
 
 // Module imports
 import ReactEmoji from "react-emoji";
+import Linkify from 'react-linkify';
+import * as linkify from 'linkifyjs';
+import hashtag from 'linkifyjs/plugins/hashtag';
 
 // Styles imports
 import "./chat.scss";
 import { useReducer } from "react";
+import ReactLinkify from "react-linkify";
 
+
+hashtag(linkify);
+    
+  
 // Component that styles the message according to the sender
 const Message = ({ message: { user, text }, name }) => {
   let is_sent_by_current_user = false; // Boolean checking if sender iscurrent user
@@ -36,31 +44,53 @@ const Message = ({ message: { user, text }, name }) => {
   if (staff.indexOf(user) > -1) {
     is_staff = true;
   }
+  // var linkifyOptions = 
+  //   {
+  //       formatHref: function (href, type) {
+  //         if (type === 'hashtag') {
+  //           href = 'https://twitter.com/hashtag/' + href.substring(1);
+  //         }
+  //         return href;
+  //       }
+        
+  //     };
 
+      
   return is_sent_by_current_user ? (
+    
     <div className="message-container justify-end">
       <p className="sent-text pr-10">{trimmed_name} </p>
       <div className="message-box background-dark">
-        <p className="message-text color-white">{ReactEmoji.emojify(text)}</p>
+        {/* <Linkify Options={linkifyOptions}> */}
+        <Linkify>
+        <p  className="message-text color-white">{ReactEmoji.emojify(text)}</p>
+        </Linkify>
+        
       </div>
     </div>
   ) : is_admin ? (
     <div className="message-container justify-middle">
       <div className="message-box-admin background-white">
+      <Linkify>
         <p className="sent-text">{ReactEmoji.emojify(text)}</p>
+        </Linkify>
       </div>
     </div>
   ) : is_staff ? (
     <div className="message-container justify-start">
       <div className="message-box background-staff">
+      <Linkify>
         <p className="message-text color-white">{ReactEmoji.emojify(text)}</p>
+        </Linkify>
       </div>
       <p className="sent-text pl-10">{user} </p>
     </div>
   ) : (
     <div className="message-container justify-start">
       <div className="message-box background-light">
+      <Linkify>
         <p className="message-text color-dark">{ReactEmoji.emojify(text)}</p>
+        </Linkify>
       </div>
       <p className="sent-text pl-10">{user} </p>
     </div>
