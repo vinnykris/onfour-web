f3f5cb725dd941889e0ff44e6a4c5a778fb539cb
LAPTOP-F15JHGDS\16939 <yuxinz9781@gmail.com>
2020-08-21 17:54:28 -0700

    VIDEO CHAT | fixed the border not showing up for camera-off users issue

--- src/components/video_chat/Tile/Tile.js
+++ src/components/video_chat/Tile/Tile.js
@@ -49,7 +49,7 @@ export default function Tile(props) {
 
   function getVideoComponent() {
     return (
-      props.videoTrack && <video autoPlay id={video_component_id} muted playsInline ref={videoEl} />
+      props.videoTrack && <video autoPlay muted playsInline ref={videoEl} />
     );
   }
 
@@ -120,24 +120,23 @@ export default function Tile(props) {
     }
   }
 
-  return (
-    !props.isArtist ? (
-      <div className={getClassNames()}>
-        <div className="background" />
-        {/* {!props.isLocalPerson ? (
+  return !props.isArtist ? (
+    <div className={getClassNames()} id={video_component_id}>
+      <div className="background" />
+      {/* {!props.isLocalPerson ? (
           <div> */}
-            {props.artistView? (
-              <i 
-                className="fa fa-microphone-slash mute-others-icon" 
-                id={random_id} 
-              ></i>
-            ) : (
-              <i
-                className="fa fa-microphone-slash mute-others-icon"
-                id={fans_microphone_id}
-              ></i>
-            )}
-            {/* <div className="range-slider-container">
+      {props.artistView ? (
+        <i
+          className="fa fa-microphone-slash mute-others-icon"
+          id={random_id}
+        ></i>
+      ) : (
+        <i
+          className="fa fa-microphone-slash mute-others-icon"
+          id={fans_microphone_id}
+        ></i>
+      )}
+      {/* <div className="range-slider-container">
               <RangeSlider
                 value={volume_value}
                 onChange={changeEvent => {
@@ -146,15 +145,12 @@ export default function Tile(props) {
                 }}
               />
             </div> */}
-          {/* </div>
+      {/* </div>
         ):null} */}
-        <div className="video-call-participant-name">
-          {props.username}
-        </div>
-        {getLoadingComponent()}
-        {getVideoComponent()}
-        {getAudioComponent()}
-      </div>
-    ) : null
-  );
+      <div className="video-call-participant-name">{props.username}</div>
+      {getLoadingComponent()}
+      {getVideoComponent()}
+      {getAudioComponent()}
+    </div>
+  ) : null;
 }
