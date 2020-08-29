f740587e5c2b7b605d322cf5f36db99749cb351f
Vinod Krishnamurthy <vinodkrishnam@gmail.com>
2020-08-28 20:13:33 -0700

    LANDING | added video and tabs and general structure for landing page

--- src/components/about_page/about_page.jsx
+++ src/components/about_page/about_page.jsx
@@ -11,10 +11,89 @@ import Auth from "../../apis/UserPool";
 // Styling Imports
 import "./about_styles.scss";
 
+// Image imports
+import wave_blue from "../../images/backgrounds/wave_1.svg";
+import artist_background from "../../images/backgrounds/artist_info.png";
+import fan_background from "../../images/backgrounds/fan_info.png";
+import ReactPlayer from "react-player";
+
 //Amplify.configure(awsmobile);
 
 // AboutPage component that contains all the about page layout
 const LandingPage = () => {
-  return <div className="about-page-content"></div>;
+  const [show_artist, setShowArtist] = useState(true);
+  return (
+    <div className="about-page-content">
+      <div className="about-section">
+        <div className="background-about-container">
+          <img src={wave_blue} className="wave" />
+        </div>
+      </div>
+      <div className="about-section experience-section">
+        <div className="background-about-container">
+          {show_artist ? (
+            <div className="background-about-inner">
+              <img src={artist_background} className="info-card" />
+              <span className="header-4 tab-title artist-tab">
+                I'm an artist
+              </span>
+              <span
+                className="header-4 tab-title fan-tab not-selected"
+                onClick={() => setShowArtist(!show_artist)}
+              >
+                I'm a fan
+              </span>
+              <div className="experience-video-wrapper">
+                <ReactPlayer
+                  className="experience-video"
+                  url="https://youtu.be/82UqPEJyxZQ"
+                  width="88.333%"
+                  height="88.333%"
+                />
+              </div>
+            </div>
+          ) : (
+            <div className="background-about-inner">
+              <img src={fan_background} className="info-card" />
+              <span
+                className="header-4 tab-title artist-tab not-selected"
+                onClick={() => setShowArtist(!show_artist)}
+              >
+                I'm an artist
+              </span>
+              <span className="header-4 tab-title fan-tab">I'm a fan</span>
+              <div className="experience-video-wrapper">
+                <ReactPlayer
+                  className="experience-video"
+                  url="https://youtu.be/82UqPEJyxZQ"
+                  width="88.333%"
+                  height="88.333%"
+                />
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="about-section how-it-works-section">
+        <div className="header-2">How It Works</div>
+        <div className="step-card-container">
+          <div className="step-card"></div>
+          <div className="step-card"></div>
+          <div className="step-card"></div>
+        </div>
+      </div>
+
+      <div className="about-section feature-section">
+        <div className="header-2">This Isn't Just Another Livestream</div>
+        <div className="feature-card-container">
+          <div className="feature-row">
+            <div className="feature-card"></div>
+            <div className="feature-card"></div>
+            <div className="feature-card"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default LandingPage;
