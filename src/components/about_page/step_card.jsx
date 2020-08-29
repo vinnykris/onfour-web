f4ea516247feb36ec939f3dfe9faef3003bdc276
Vinod Krishnamurthy <vinodkrishnam@gmail.com>
2020-08-28 20:14:14 -0700

    LANDING CARDS | added empty component for how it works card

---
+++ src/components/about_page/step_card.jsx
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+
+const StepCard = ({ title, description }) => {
+  return <div className="card-container"></div>;
+};
+
+export default StepCard;
