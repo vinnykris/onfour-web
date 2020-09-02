1477f6d4c20a76c6d5c161268fd1d5c684279cfb
Vinod Krishnamurthy <vinodkrishnam@gmail.com>
2020-09-01 22:32:18 -0700

    ADMIN CHAT | removed height from admin chat class

--- src/components/chat/message.jsx
+++ src/components/chat/message.jsx
@@ -57,13 +57,13 @@ const Message = ({ message: { user, text }, name }) => {
 
   return is_sent_by_current_user ? (
     <div className="message-container justify-start">
-      <h5 className={"body-2 sent-text pr-10" + " username-color-" + color_id}>{trimmed_name + ":"} </h5>
+      <h5 className={"body-2 sent-text pr-10" + " username-color-" + color_id}>
+        {trimmed_name + ":"}{" "}
+      </h5>
       <div className="message-box">
         {/* <Linkify Options={linkifyOptions}> */}
         <Linkify>
-          <p className="body-2 chat-message-text">
-            {ReactEmoji.emojify(text)}
-          </p>
+          <p className="body-2 chat-message-text">{ReactEmoji.emojify(text)}</p>
         </Linkify>
       </div>
     </div>
@@ -71,29 +71,29 @@ const Message = ({ message: { user, text }, name }) => {
     <div className="message-container justify-middle">
       <div className="message-box-admin">
         <Linkify>
-          <p className="body-2 sent-text">{ReactEmoji.emojify(text)}</p>
+          <p className="body-2 admin-sent-text">{ReactEmoji.emojify(text)}</p>
         </Linkify>
       </div>
     </div>
   ) : is_staff ? (
     <div className="message-container justify-start">
-      <h5 className={"body-2 sent-text pr-10" + " username-color-" + color_id}>{user + ":"} </h5>
+      <h5 className={"body-2 sent-text pr-10" + " username-color-" + color_id}>
+        {user + ":"}{" "}
+      </h5>
       <div className="message-box">
         <Linkify>
-          <p className="body-2 chat-message-text">
-            {ReactEmoji.emojify(text)}
-          </p>
+          <p className="body-2 chat-message-text">{ReactEmoji.emojify(text)}</p>
         </Linkify>
       </div>
     </div>
   ) : (
     <div className="message-container justify-start">
-      <h5 className={"body-2 sent-text pr-10" + " username-color-" + color_id}>{user + ":"} </h5>
+      <h5 className={"body-2 sent-text pr-10" + " username-color-" + color_id}>
+        {user + ":"}{" "}
+      </h5>
       <div className="message-box">
         <Linkify>
-          <p className="body-2 chat-message-text">
-            {ReactEmoji.emojify(text)}
-          </p>
+          <p className="body-2 chat-message-text">{ReactEmoji.emojify(text)}</p>
         </Linkify>
       </div>
     </div>
