4ed656b2470a7039298c8e3e26c06949291d184d
LAPTOP-F15JHGDS\16939 <yuxinz9781@gmail.com>
2020-09-13 14:43:17 -0700

    FOOTER | changed font family

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -47,217 +47,256 @@ const Footer = () => {
     <div>
       {show_footer ? (
         <div>
-        {width > 600 ?
-          (
-            
-            <div className="footer-container">
-          <div className="footer-inner-container">
-            <div className="footer-column-main">
-              <img className="onfour-logo-container" src={desktop_icon}></img>
-              <span className="header-8 footer-text-body">
-                Experience music, together.
-              </span>
+        {width > 600 ?(  
+          <div className="footer-container">
+            <div className="footer-inner-container">
+              <div className="footer-column-main">
+                <img className="onfour-logo-container" src={desktop_icon}></img>
+                <span className="header-8 footer-text-body">
+                  Experience music, together.
+                </span>
+              </div>
+              <div className="footer-link-container">
+                <div className="footer-column-content">
+                  <span className="header-8 footer-text-title">Explore</span>
+                  <NavLink
+                    className="header-8 footer-text-body footer-link"
+                    to="/home"
+                  >
+                    Home
+                  </NavLink>
+                  <NavLink
+                    className="header-8 footer-text-body footer-link"
+                    to="/"
+                  >
+                    About Us
+                  </NavLink>
+                  <NavLink
+                    className="header-8 footer-text-body footer-link"
+                    to="/stream"
+                  >
+                    Stream
+                  </NavLink>
+                  <NavLink
+                    className="header-8 footer-text-body footer-link"
+                    to="/upcoming"
+                  >
+                    Upcoming
+                  </NavLink>
+                </div>
+                <div className="footer-column-content">
+                  <span className="header-8 footer-text-title">Legal</span>
+                  <NavLink
+                    className="header-8 footer-text-body footer-link"
+                    to="/terms-of-service"
+                  >
+                    Terms of Service
+                  </NavLink>
+                  <NavLink
+                    className="header-8 footer-text-body footer-link"
+                    to="/privacy-policy"
+                  >
+                    Privacy Policy
+                  </NavLink>
+                  <NavLink
+                    className="header-8 footer-text-body footer-link"
+                    to="/ticket-agreement"
+                  >
+                    Ticket Agreement
+                  </NavLink>
+                </div>
+                <div className="footer-column-content">
+                  <span className="header-8 footer-text-title">Collaborate</span>
+                  <a
+                    onClick={() => Analytics.record({ name: "contactUsFooter" })}
+                    href="mailto:onfour.box@gmail.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="header-8 footer-text-body footer-link"
+                  >
+                    onfour.box@gmail.com
+                  </a>
+                </div>
+              </div>
             </div>
-            <div className="footer-link-container">
-              <div className="footer-column-content">
-                <span className="header-8 footer-text-title">Explore</span>
-                <NavLink
-                  className="header-8 footer-text-body footer-link"
-                  to="/home"
-                >
-                  Home
-                </NavLink>
-                <NavLink
-                  className="header-8 footer-text-body footer-link"
-                  to="/"
-                >
-                  About Us
-                </NavLink>
-                <NavLink
-                  className="header-8 footer-text-body footer-link"
-                  to="/stream"
-                >
-                  Stream
-                </NavLink>
-                <NavLink
-                  className="header-8 footer-text-body footer-link"
-                  to="/upcoming"
-                >
-                  Upcoming
-                </NavLink>
-              </div>
-              <div className="footer-column-content">
-                <span className="header-8 footer-text-title">Legal</span>
-                <NavLink
-                  className="header-8 footer-text-body footer-link"
-                  to="/terms-of-service"
-                >
-                  Terms of Service
-                </NavLink>
-                <NavLink
-                  className="header-8 footer-text-body footer-link"
-                  to="/privacy-policy"
-                >
-                  Privacy Policy
-                </NavLink>
-                <NavLink
-                  className="header-8 footer-text-body footer-link"
-                  to="/ticket-agreement"
-                >
-                  Ticket Agreement
-                </NavLink>
-              </div>
-              <div className="footer-column-content">
-                <span className="header-8 footer-text-title">Collaborate</span>
-                <a
-                  onClick={() => Analytics.record({ name: "contactUsFooter" })}
-                  href="mailto:onfour.box@gmail.com"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="header-8 footer-text-body footer-link"
-                >
-                  onfour.box@gmail.com
-                </a>
+            <div className="footer-inner-container">
+              <div className="footer-bottom">
+                <span className="header-8 footer-text-body">
+                  Copyright &copy; 2020 All Rights Reserved by onfour
+                </span>
+                <div className="footer-social-media">
+                  <ul>
+                    <li>
+                      <a
+                        onClick={() => Analytics.record({ name: "instaFooter" })}
+                        href="https://www.instagram.com/_onfour/"
+                        target="_blank"
+                        className="fa fa-instagram"
+                        rel="noopener noreferrer"
+                      >
+                        <span className="hidden-text">Instagram Link</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        onClick={() =>
+                          Analytics.record({ name: "youtubeFooter" })
+                        }
+                        href="https://www.youtube.com/channel/UCwbWryexV1632eZ_pILnmTQ/featured"
+                        target="_blank"
+                        className="fa fa-youtube"
+                        rel="noopener noreferrer"
+                      >
+                        <span className="hidden-text">Youtube Link</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        onClick={() =>
+                          Analytics.record({ name: "twitterFooter" })
+                        }
+                        href="https://twitter.com/_Onfour"
+                        target="_blank"
+                        className="fa fa-twitter"
+                        rel="noopener noreferrer"
+                      >
+                        <span className="hidden-text">Twitter Link</span>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
-          <div className="footer-inner-container">
-            <div className="footer-bottom">
-              <span className="header-8 footer-text-body">
-                Copyright &copy; 2020 All Rights Reserved by onfour
-              </span>
-              <div className="footer-social-media">
-                <ul>
-                  <li>
-                    <a
-                      onClick={() => Analytics.record({ name: "instaFooter" })}
-                      href="https://www.instagram.com/_onfour/"
-                      target="_blank"
-                      className="fa fa-instagram"
-                      rel="noopener noreferrer"
-                    >
-                      <span className="hidden-text">Instagram Link</span>
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      onClick={() =>
-                        Analytics.record({ name: "youtubeFooter" })
-                      }
-                      href="https://www.youtube.com/channel/UCwbWryexV1632eZ_pILnmTQ/featured"
-                      target="_blank"
-                      className="fa fa-youtube"
-                      rel="noopener noreferrer"
-                    >
-                      <span className="hidden-text">Youtube Link</span>
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      onClick={() =>
-                        Analytics.record({ name: "twitterFooter" })
-                      }
-                      href="https://twitter.com/_Onfour"
-                      target="_blank"
-                      className="fa fa-twitter"
-                      rel="noopener noreferrer"
-                    >
-                      <span className="hidden-text">Twitter Link</span>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          
-        </div>
-  
-          ):(
-            <div className="footer-container">
+        ):(
+          <div className="footer-container mobile-footer">
           <Grid className="footer-grid">
-            <Row>
-              
+            <Row> 
               <Col size={1}>
-              <div className="footer-logo-container">
+                <div className="mobile-footer-logo-container">
                   <img
-                    className="onfour-logo-footer"
+                    className="mobile-onfour-logo-footer"
                     src={mobile_icon}
                     width="auto"
-                    height="50px"
+                    height="49px"
                     alt="nav-logo"
                   ></img>
                 </div>
-                <div className="onfour-logo-subheading">
-                  <h4>Experience Music Together</h4>
+                <div className="mobile-onfour-logo-subheading">
+                  <h4 className="header-9">Experience Music Together</h4>
                 </div>
-                
               </Col>
             </Row>
-            {/* <Row>
-              <Col size={0.5}></Col>
-              <Col size={1}>
-              <div className="onfour-logo-subheading">
-                  <h3>Experience Music Together</h3>
-                </div>
-              </Col>
-            </Row> */}
-            
-            
             <Row>
-            <Col size={0.5}></Col>
-              <Col size={1.5}>
+              <div className="mobile-footer-link-container">
+                <div className="mobile-footer-column-content">
+                  <span className="header-10 footer-text-title">Explore</span>
+                  <NavLink
+                    className="header-10 footer-text-body mobile-footer-link"
+                    to="/home"
+                  >
+                    Home
+                  </NavLink>
+                  <NavLink
+                    className="header-10 footer-text-body mobile-footer-link"
+                    to="/"
+                  >
+                    About Us
+                  </NavLink>
+                  <NavLink
+                    className="header-10 footer-text-body mobile-footer-link"
+                    to="/stream"
+                  >
+                    Stream
+                  </NavLink>
+                  <NavLink
+                    className="header-10 footer-text-body mobile-footer-link"
+                    to="/upcoming"
+                  >
+                    Upcoming
+                  </NavLink>
+                </div>
+                <div className="mobile-footer-column-content">
+                  <span className="header-10 footer-text-title">Legal</span>
+                  <NavLink
+                    className="header-10 footer-text-body mobile-footer-link"
+                    to="/terms-of-service"
+                  >
+                    Terms of Service
+                  </NavLink>
+                  <NavLink
+                    className="header-10 footer-text-body mobile-footer-link"
+                    to="/privacy-policy"
+                  >
+                    Privacy Policy
+                  </NavLink>
+                  <NavLink
+                    className="header-10 footer-text-body mobile-footer-link"
+                    to="/ticket-agreement"
+                  >
+                    Ticket Agreement
+                  </NavLink>
+                </div>
+                <div className="mobile-footer-column-content last-mobile-footer-column-content">
+                  <span className="header-10 footer-text-title">Collaborate</span>
+                  <a
+                    onClick={() => Analytics.record({ name: "contactUsFooter" })}
+                    href="mailto:onfour.box@gmail.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="header-10 footer-text-body mobile-footer-link"
+                  >
+                    onfour.box@gmail.com
+                  </a>
+                </div>
+              </div>
+              {/* <Col size={1}>
                 <div className="footer-links">
-                  <h3>Explore</h3>
+                  <div className="header-10 mobile-column-title">Explore</div>
                   <ul>
                     <li>
-                      <a href="/about" >
+                      <a href="/about" className="header-10">
                         About Us
                       </a>
                     </li>
-                  
                     <li>
-                      <a href="/artiststream"
-                      >
+                      <a href="/artiststream" className="header-10">
                       Stream
                       </a>
                       </li>
                     <li>
-                      <a href="/upcoming">
+                      <a href="/upcoming" className="header-10">
                       Upcoming
                       </a>
                       </li>
                   </ul>
                 </div>
-                </Col>
-                <Col size={0.5}></Col>
-                <Col size={2}>
+              </Col>
+              <Col size={2}>
                 <div className="footer-links">
-                  <h3>Legal</h3>
+                  <div className="header-10 mobile-column-title">Legal</div>
                   <ul>
-                  <li>
-                      <NavLink to="/terms-of-service" className="privacy-link">
+                    <li>
+                      <NavLink to="/terms-of-service" className="privacy-link header-10">
                         Terms of Service
                       </NavLink>
                     </li>
                     <li>
-                      <NavLink to="/privacy-policy" className="privacy-link">
+                      <NavLink to="/privacy-policy" className="privacy-link header-10">
                         Privacy Policy
                       </NavLink>
                     </li>
                     <li>
-                      <NavLink to="/ticket-agreement" className="privacy-link">
+                      <NavLink to="/ticket-agreement" className="privacy-link header-10">
                         Ticket Agreement
                       </NavLink>
                     </li>
                   </ul>
                 </div>
               </Col>
-              
-              <Col size={0.5}>
-              </Col>
               <Col size={2}>
                 <div className="footer-links">
-                  <h3>Collaborate</h3>
+                  <div className="header-10 mobile-column-title">Collaborate</div>
                   <ul>
                     <li>
                       <a
@@ -267,29 +306,20 @@ const Footer = () => {
                         href="mailto:onfour.box@gmail.com"
                         target="_blank"
                         rel="noopener noreferrer"
+                        className="header-10"
                       >
                         onfour.box@gmail.com
                       </a>
                     </li>
-                    <li>
-                      <a
-                       
-                      href="tel:+496170961709"
-                      >
-                        123.356.2356
-                      </a>
-                    </li>
-                    
                   </ul>
                 </div>
-              </Col>
-              <Col size={0.5}></Col>
+              </Col> */}
             </Row>
-            <hr></hr>
+            {/* <hr></hr>
             <Row>
               <Col size={0.2}></Col>
               <Col size={5}>
-                <h6>Copyright &copy; 2020 All Rights Reserved by onfour</h6>
+                <h6 className="header-10">Copyright &copy; 2020 All Rights Reserved by onfour</h6>
               </Col>
               <Col size={0.2}></Col>
               <Col size={3}>
@@ -338,12 +368,11 @@ const Footer = () => {
                 </div>
               </Col>
               <Col size={0.2}></Col>
-            </Row>
+            </Row> */}
           </Grid>
         </div>
-
-          )}
-        </div>
+      )}
+      </div>
       ) : null}
     </div>
   );
