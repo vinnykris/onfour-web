884c0aee99882fd52a636fb8bff1841f407efa37
LAPTOP-F15JHGDS\16939 <yuxinz9781@gmail.com>
2020-09-18 16:33:23 -0700

    404 | finished 404 page redesign for both desktop and mobile

--- src/components/not_found_page/not_found_page.jsx
+++ src/components/not_found_page/not_found_page.jsx
@@ -1,32 +1,107 @@
 import "../not_found_page/not_found_page_style.scss";
-import history from "../../history";
+import { useWindowDimensions } from "../custom_hooks";
+//AWS
+import { Analytics } from "aws-amplify";
+
 var React = require("react");
 
+
 const NotFoundPage = () => {
-  const goHome = () => {
-    history.push("/");
-  };
+  const { height, width } = useWindowDimensions(); // Dimensions of screen
 
   return (
     <div className="not-found-page">
+      <img
+        src="https://onfour-media.s3.amazonaws.com/website+component/404+without+text.jpg"
+        className="not-found-page-img404"
+      />
       <div className="not-found-page-container">
-        <img
-          src="https://onfour-media.s3.amazonaws.com/website+component/404image.png"
-          className="not-found-page-img404"
-        />
         <div className="not-found-page-text">
-          <h1 className="not-found-page-oops-heading">OOPS! Page Not Found</h1>
-          <h3 className="not-found-page-oops-error-text">
-            The page you are trying to reach does not exist (yet). We're glad to
-            see that you're exploring but just in case, please check that the
-            URL you entered is correct.
+          <h1 className={"not-found-page-oops-heading " + (width > 600 ? "header-2" : "header-4")}>Don't Panic!</h1>
+          <h3 className={"not-found-page-oops-error-text " + (width > 600 ? "header-8" : "subtitle-3")}>
+            {"You found a rare 404 page in a different dimension \nShare your discovery with friends!"}
           </h3>
           <div className="not-found-page-button-wrapper">
-            <button className="not-found-page-go-back-button" onClick={goHome}>
-              Go Home
-            </button>
+            <div className="mobile-footer-social-media">
+              <ul className="social-list">
+                <li>
+                  <a
+                    onClick={() =>
+                      Analytics.record({
+                        name: "404Insta",
+                      })
+                    }
+                    href="https://www.instagram.com/_onfour/"
+                    className="fa fa-instagram"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <span>Instagram Link</span>
+                  </a>
+                </li>
+                {/* <li>
+                  <a
+                    onClick={() =>
+                      Analytics.record({
+                        name: "404Spotify",
+                      })
+                    }
+                    href={artist_spotify}
+                    className="fab fa-spotify"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <span>Spotify Link</span>
+                  </a>
+                </li> */}
+                <li>
+                  <a
+                    onClick={() =>
+                      Analytics.record({
+                        name: "404Youtube",
+                      })
+                    }
+                    href="https://www.youtube.com/channel/UCwbWryexV1632eZ_pILnmTQ/featured"
+                    className="fa fa-youtube"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <span>Youtube Link</span>
+                  </a>
+                </li>
+                {/* <li>
+                  <a
+                    onClick={() =>
+                      Analytics.record({
+                        name: "404Facebook",
+                      })
+                    }
+                    href={artist_fb}
+                    className="fab fa-facebook"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <span>Facebook Link</span>
+                  </a>
+                </li> */}
+                <li>
+                  <a
+                    onClick={() =>
+                      Analytics.record({
+                        name: "socialBarTwitter",
+                      })
+                    }
+                    href="https://twitter.com/_Onfour"
+                    className="fa fa-twitter"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <span>Twitter Link</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
-          {/* <a href="/" className="go-back-button">Go Back</a> */}
         </div>
       </div>
     </div>
