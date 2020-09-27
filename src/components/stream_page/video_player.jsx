bcb0c7fd8abde6e245c8e3bf126bf453e7eb0b3a
Vinod Krishnamurthy <vinodkrishnam@gmail.com>
2020-09-27 15:33:30 -0700

    MINOR UPDATES | added invite prompt to waiting screen, and changed copy slightly for rejoining private room

--- src/components/stream_page/video_player.jsx
+++ src/components/stream_page/video_player.jsx
@@ -296,18 +296,22 @@ function VideoPlayer({
           {width <= 600 ? (
             <div className="waiting-message-container">
               <h3 className="header-4">Next Stream Coming Soon</h3>
-              {/* <h5 className="waiting-message2">For updates, follow us on Instagram @_onfour</h5> */}
+              <h5 className="waiting-message2 header-7">
+                Invite your friends to join you while waiting!
+              </h5>
               <h5 className="header-7">
-                {artist_name}
+                UP NEXT: {artist_name}
                 {/* - {concert_name} */}
               </h5>
             </div>
           ) : (
             <div className="waiting-message-container">
               <h3 className="header-3">Next Stream Coming Soon</h3>
-              {/* <h5 className="waiting-message2">For updates, follow us on Instagram @_onfour</h5> */}
+              <h5 className="waiting-message2 header-5">
+                Invite your friends to join you while waiting!
+              </h5>
               <h5 className="header-5">
-                {artist_name}
+                UP NEXT: {artist_name}
                 {/* - {concert_name} */}
               </h5>
             </div>
