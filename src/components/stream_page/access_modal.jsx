186b2f85c15333cce03f3c7486fb8263efc0de4a
Vinod Krishnamurthy <vinodkrishnam@gmail.com>
2020-10-30 16:55:56 -0700

    BASIC WORKING EDITION | works in most basic case where user has ticket already

--- src/components/stream_page/access_modal.jsx
+++ src/components/stream_page/access_modal.jsx
@@ -1,81 +1,90 @@
-import React, { useState, useEffect } from "react";
-import Rodal from "rodal";
+import React, { useState, useEffect } from 'react'
+import Rodal from 'rodal'
 
-import "./access_modal_styles.scss";
+import './access_modal_styles.scss'
 
-import { useInputValue, useWindowDimensions } from "../custom_hooks";
+import { useInputValue, useWindowDimensions } from '../custom_hooks'
 
-import InputOne from "../inputs/input_one";
+import InputOne from '../inputs/input_one'
 
-const AccessModal = (props) => {
-  const [show_modal, setShowModal] = useState(true);
-  const email_input = useInputValue("");
+const AccessModal = props => {
+  const [show_modal, setShowModal] = useState(true)
+  const email_input = useInputValue('')
 
   const closeModal = () => {
-    setShowModal(false);
-  };
+    setShowModal(false)
+  }
 
-  const handleSubmit = () => {
-    console.log(email_input);
-  };
+  const handleSubmit = event => {
+    event.preventDefault()
+    props.checkAccess(email_input.value)
+    console.log(email_input)
+  }
   return (
     <div>
       <Rodal
         visible={props.visible}
         width={482}
         height={424}
-        measure="px"
+        measure='px'
         onClose={props.onClose}
         customStyles={{
           padding: 0,
           //overflow: scroll,
-          maxHeight: "545px",
-          maxWidth: "482px",
+          maxHeight: '545px',
+          maxWidth: '482px',
           background:
-            "linear-gradient(0deg, rgba(255, 255, 255, 0.09), rgba(255, 255, 255, 0.09)), #07070F",
+            'linear-gradient(0deg, rgba(255, 255, 255, 0.09), rgba(255, 255, 255, 0.09)), #07070F',
           boxShadow:
-            "0px 4px 5px rgba(0, 0, 0, 0.14), 0px 1px 10px rgba(0, 0, 0, 0.12), 0px 2px 4px rgba(0, 0, 0, 0.2)",
-          borderRadius: "10px",
+            '0px 4px 5px rgba(0, 0, 0, 0.14), 0px 1px 10px rgba(0, 0, 0, 0.12), 0px 2px 4px rgba(0, 0, 0, 0.2)',
+          borderRadius: '10px'
         }}
         closeMaskOnClick={false}
         showCloseButton={false}
       >
-        <div className="access-modal-main">
-          <div className="access-modal-header">
-            <span className="access-modal-title header-5">Enter Venue</span>
+        <div className='access-modal-main'>
+          <div className='access-modal-header'>
+            <span className='access-modal-title header-5'>Enter Venue</span>
           </div>
-          <form className="access-modal-body" onSubmit={handleSubmit}>
-            <div className="enter-email-prompt">
-              <span className="prompt-text subtitle-1">
+          <form className='access-modal-body' onSubmit={handleSubmit}>
+            <div className='enter-email-prompt'>
+              <span className='prompt-text subtitle-1'>
                 If you have purchased your ticket already, please enter your
                 email to enter the venue:
               </span>
             </div>
-            <div className="enter-email-input">
+            <div className='enter-email-input'>
               <InputOne
-                id="stream-email-input"
-                type="email"
-                name="access-email"
+                id='stream-email-input'
+                type='email'
+                name='access-email'
                 is_required={true}
                 {...email_input}
-                placeholder="Email"
+                placeholder='Email'
               />
             </div>
-            <div className="submit-email-button-container">
+            <div className='submit-email-button-container'>
               <button
-                className="primary-button enter-venue-button"
-                type="submit"
+                className='primary-button enter-venue-button'
+                type='submit'
               >
-                <span className="button-text">Enter</span>
+                <span className='button-text'>Enter</span>
               </button>
             </div>
+            {props.access_error ? (
+              <div className='failed-access-message-container'>
+                <span className='failed-access-message header-8'>
+                  We can't seem to find your ticket.
+                </span>
+              </div>
+            ) : null}
           </form>
-          <div className="buy-ticket-prompt-container">
-            <span className="buy-ticket-prompt header-7">
+          <div className='buy-ticket-prompt-container'>
+            <span className='buy-ticket-prompt header-7'>
               Don't have a ticket? Click&nbsp;
               <span
                 onClick={props.openTicketModal}
-                className="open-ticket-modal-link header-7"
+                className='open-ticket-modal-link header-7'
               >
                 here
               </span>
@@ -85,7 +94,7 @@ const AccessModal = (props) => {
         </div>
       </Rodal>
     </div>
-  );
-};
+  )
+}
 
-export default AccessModal;
+export default AccessModal
